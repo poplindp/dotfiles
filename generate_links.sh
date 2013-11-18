387b74723af481746ea0709239e8b5e9880683dd
diego <diego.pantano@gmail.com>
2013-11-18 23:59:51 +0100

    PWD path in generate_links.sh

--- generate_links.sh
+++ generate_links.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
-ln -s {_,${HOME}/.}Xresources
-ln -s {_,${HOME}/.}vimrc
-ln -s {_,${HOME}/.}zshrc
+ln -s {${PWD}/_,${HOME}/.}Xresources
+ln -s {${PWD}/_,${HOME}/.}vimrc
+ln -s {${PWD}/_,${HOME}/.}zshrc
