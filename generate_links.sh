701aa3eb841fdca0736b17a81fe90b976d21d2e4
diego <diego.pantano@gmail.com>
2013-11-19 12:00:31 +0100

    new generate_links.sh, add zprofile

--- generate_links.sh
+++ generate_links.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-ln -s {${PWD}/_,${HOME}/.}Xresources
-ln -s {${PWD}/_,${HOME}/.}vimrc
-ln -s {${PWD}/_,${HOME}/.}zshrc
-ln -s {${PWD}/_,${HOME}/.}tmux.conf
+
+for dotfile in _*
+do 
+	if [ ! -L ${HOME}/.${dotfile##_} ]
+	then
+		echo "Generating symbolic link ${HOME}/.${dotfile##_} to ${PWD}/${dotfile}"
+		ln -s {${PWD}/_,${HOME}/.}${dotfile##_}
+	else
+		echo "Skipping existant ${HOME}/.${dotfile##_}"
+	fi
+done
+
+#ln -s {${PWD}/_,${HOME}/.}Xresources
+#ln -s {${PWD}/_,${HOME}/.}vimrc
+#ln -s {${PWD}/_,${HOME}/.}zshrc
+#ln -s {${PWD}/_,${HOME}/.}zprofile
+#ln -s {${PWD}/_,${HOME}/.}tmux.conf
