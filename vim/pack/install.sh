0ab8dba090256d964b8d7a6de1b1d3ef09d9d24c
diego <diego.pantano@gmail.com>
2017-08-20 14:55:10 +0200

    just revert modif of .gitignore.  pack dir is not tracked only update.sh and install.sh

---
+++ vim/pack/install.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+
+# Create new folder in ~/.vim/pack that contains a start folder and cd into it.
+#
+# Arguments:
+#   package_group, a string folder name to create and change into.
+#
+# Examples:
+#   set_group syntax-highlighting
+#
+function set_group () {
+  package_group=$1
+  path="$HOME/.vim/pack/$package_group/start"
+  mkdir -p "$path"
+  cd "$path" || exit
+}
+
+function set_group_opt () {
+  package_group=$1
+  path="$HOME/.vim/pack/$package_group/opt"
+  mkdir -p "$path"
+  cd "$path" || exit
+}
+
+# Clone or update a git repo in the current directory.
+#
+# Arguments:
+#   repo_url, a URL to the git repo.
+#
+# Examples:
+#   package https://github.com/tpope/vim-endwise.git
+#
+function package () {
+  if [[ "$repo_url" != http* ]]
+  then
+    repo_url="https://github.com/${repo_url}"
+  fi
+  repo_url=$1
+  expected_repo=$(basename "$repo_url" .git)
+  if [ -d "$expected_repo" ]; then
+    cd "$expected_repo" || exit
+    result=$(git pull --force)
+    echo "$expected_repo: $result"
+  else
+    echo "$expected_repo: Installing..."
+    git clone -q "$repo_url"
+  fi
+}
+
+(
+set_group staging
+package fatih/vim-go.git &
+package tpope/vim-rhubarb.git &
+package dhruvasagar/vim-table-mode.git &
+wait
+) &
+
+(
+set_group tpope
+# package tpope/vim-dispatch.git &
+# package tpope/vim-jdaddy.git &
+package tpope/vim-fugitive.git &
+package tpope/vim-surround.git &
+package tpope/vim-ragtag.git &
+package tpope/vim-abolish.git &
+package tpope/vim-repeat.git &
+package tpope/vim-commentary.git &
+package tpope/vim-projectionist.git &
+# package tpope/vim-vinegar.git &
+wait
+) &
+
+# (
+# set_group ruby
+# package tpope/vim-rails.git &
+# package tpope/vim-rake.git &
+# package tpope/vim-bundler.git &
+# package tpope/vim-endwise.git &
+# wait
+# ) &
+
+(
+set_group snippets
+# Vim-snipmate and dependencies - VimL but buggy
+package MarcWeber/vim-addon-mw-utils.git &
+package tomtom/tlib_vim.git &
+package garbas/vim-snipmate.git &
+# Ultisnips is python but more functional
+package SirVer/ultisnips.git &
+wait
+) &
+
+(
+set_group tmux
+package christoomey/vim-tmux-navigator.git &
+wait
+) &
+
+(
+set_group syntax
+package kchmck/vim-coffee-script.git &
+package tpope/vim-markdown.git &
+package ap/vim-css-color.git &
+package pearofducks/ansible-vim.git &
+package vim-scripts/applescript.vim.git &
+package tmux-plugins/vim-tmux.git &
+package Matt-Deacalion/vim-systemd-syntax.git &
+package salomvary/vim-eslint-compiler.git &
+package hashivim/vim-terraform.git &
+wait
+) &
+
+
+(
+set_group colorschemes
+package altercation/vim-colors-solarized.git &
+wait
+) &
+
+wait
+
+# echo "Plugins that havn't been updated by this script:"
+find */*/*/.git -prune -mtime 0.01
