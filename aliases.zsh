8739ad8d2e4158fb456d3e814fb7e7beb2e6d2d8
diego <diego.pantano@gmail.com>
2017-08-20 15:05:43 +0200

    create zsh folder moving inside zshrc and aliases.zsh

--- aliases.zsh
+++
@@ -1,9 +0,0 @@
-# alias
-alias wol_nuits="ssh -p 11111 root@spqr.hd.free.fr 'ether-wake -i br0 00:30:67:07:58:5C'"
-alias grep="grep --color=always"
-alias fgrep="fgrep --color=always"
-alias egrep="egrep --color=always"
-alias ls='ls --color=auto'
-alias l.='ls -d .* --color=auto'
-alias l='ls -lthr'
-alias ll='ls -la'
