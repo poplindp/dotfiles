590b3e2cfac683e12b4b134be33eb1335f812aec
diego <diego.pantano@gmail.com>
2017-08-28 13:54:26 +0200

    new aliases.zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,8 @@
+# alias
+alias grep="grep --color=always"
+alias fgrep="fgrep --color=always"
+alias egrep="egrep --color=always"
+alias ls='ls --color=auto'
+alias l.='ls -d .* --color=auto'
+alias l='ls -lthr'
+alias ll='ls -la'
